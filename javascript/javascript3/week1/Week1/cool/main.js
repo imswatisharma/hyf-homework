e569bd483c9459e025c3f6131ee1bc7353b8e3ba
imswatisharma <49231663+imswatisharma@users.noreply.github.com>
2019-11-24 13:02:03 +0100

    Add files via upload

---
+++ javascript/javascript3/week1/Week1/cool/main.js
@@ -0,0 +1,26 @@
+const imageDiv = document.querySelector("#image_div");
+
+function photo(){
+const apiKey = "26d8d016290f13f48088af169ba901361c1bc0cc462eeacf88793d9d7b6ad510";
+fetch("https://api.unsplash.com/photos/?client_id="+ apiKey)
+.then(response =>response.json())
+.then(json => {
+  console.log(json);
+  displayPhoto(json);
+});
+}
+
+/**
+ * display images of json file
+ *
+ * @param {*} data =>data of json file
+ */
+function displayPhoto(data){
+  const urlArray = data.map(element => element.urls.regular);
+  urlArray.forEach(item => {
+    const image = document.createElement("img");
+    image.setAttribute("src",item);
+    imageDiv.appendChild(image);  
+  });
+}
+document.getElementById("btnReload").addEventListener("click", photo);
